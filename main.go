def9896635fb9dc85783355b1842a0ed524bd0cf
Tyson Taylor <ttaylordev@users.noreply.github.com>
2023-06-04 05:44:38 -0600

    basic working

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-fn main(){
+import "net/http"
+import "log"
+import "os"
 
+func main() {
+    logger := log.New(os.Stdout, "", log.LstdFlags)
+    logger.Println("Server started on http://localhost:8082")
+
+     http.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request){
+        
+        _, err := w.Write([]byte("Hello world"))
+        if err != nil {
+
+            // Handle the error
+            logger.Println("Error writing response: ", err)
+            http.Error(w, "Internal server error", http.StatusInternalServerError)
+            return
+        }
+    })
+
+    err := http.ListenAndServe(":8082", nil)
+    if err != nil{
+        logger.Fatal("Error starting server: ", err)
+    }
+    
 }
